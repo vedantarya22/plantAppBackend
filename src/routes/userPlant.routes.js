dde585b6bac1e7b65d7413610a037cadb65c2911
simran <simran.tiwari2@mitwpu.edu.in>
2026-03-07 17:07:44 +0530

    userPlant:route fix

--- src/routes/userPlant.routes.js
+++ src/routes/userPlant.routes.js
@@ -5,14 +5,14 @@ import { addUserPlant, getUserPlants, updateCare,markTaskDone,removePlant,remove
 const router = Router();
 
 router.route('/').post(addUserPlant);
-router.route('/user/:userId').get(getUserPlants);
+router.route('/user').get(getUserPlants);
 router.route('/:id/care').patch(updateCare);
 router.route('/:id/done/:taskType').patch(markTaskDone); // ← swipe to done hits this   
 
 
 // ✅ 3 delete routes
-router.route('/:id').delete(removePlant);                                    // removePlant(by id)
 router.route('/type/:plantId/site/:siteId').delete(removeAllPlantsOfType);   // removeAllPlants(plantId:siteID:)
 router.route('/site/:siteId').delete(removeSiteWithPlants);    
+router.route('/:id').delete(removePlant);                                    // removePlant(by id)
 
 export default router;
